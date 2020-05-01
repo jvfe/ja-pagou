dbd031786369e73fc8fe9e0756905bed7f66f4dd
jvfe <jvfecav@gmail.com>
2020-05-01 15:37:36 -0300

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-let fs = require("fs")
-let rita = require("rita")
+const fs = require("fs")
+const rita = require("rita")
 
 function fazDisciplina() {
-  let disciplinas = fs.readFileSync("../data/nomes_withdot.txt").toString('utf-8')
-  let disMark = new rita.RiMarkov(2)
+  const disciplinas = fs.readFileSync("../data/nomes_withdot.txt").toString('utf-8')
+  const disMark = new rita.RiMarkov(2)
   disMark.loadText(disciplinas)
-  let disciplina = disMark.generateSentences(1)
-  return disciplina[0].substring(0, disciplina[0].length - 1)
+  let disciplina = disMark.generateSentence()
+  return disciplina.substring(0, disciplina.length - 1)
 }
 
 function fazEmenta() {
-  let ementas = fs.readFileSync("../data/ementas-final1.txt").toString('utf-8')
-  let emenMark = new rita.RiMarkov(4)
+  const ementas = fs.readFileSync("../data/ementas-final1.txt").toString('utf-8')
+  const emenMark = new rita.RiMarkov(3)
   emenMark.loadText(ementas)
-  let ementa = emenMark.generateSentences(3).join("\n")
-  return "Ementa:\n"+ ementa
+  let ementa = emenMark.generateSentences(3)
+  return ementa
 }
 
-console.log(fazDisciplina())
-console.log(fazEmenta())
+function formata(){
+  const disciplina = fazDisciplina()
+  let unidades = fazEmenta()
+  unidades = unidades.map(unidade => '\t'+unidade+'\n').join('')
+  let completo = disciplina +'\n'+ 'Ementa:\n' + unidades
+  if (completo.length > 280){
+   completo = formata()
+  }
+  return completo
+}
+
+module.exports = { formata: formata }
+
+// console.log(formata())
